93acc72f98489e64e8e2b9d54bc36cfb75fe4cbf
kristineD11 <delumen.kristine.b@gmail.com>
2024-10-30 10:14:22 +0800

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,30 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  initializeAuth,
-  getReactNativePersistence,
-} from "firebase/auth";
-import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBtW0_CwQQYHOhlL65xkqpMhwLPncd1Y3g",
-  authDomain: "fir-integration-87f97.firebaseapp.com",
-  projectId: "fir-integration-87f97",
-  storageBucket: "fir-integration-87f97.appspot.com",
-  messagingSenderId: "989490278968",
-  appId: "1:989490278968:web:741006edf3e34a421eeccd",
-  measurementId: "G-KSKYK6MYM1",
+  apiKey: "AIzaSyBMCxzoGkcYxtue5EpV3JkGHrckzd0hzVk",
+  authDomain: "fir-integration-ee139.firebaseapp.com",
+  projectId: "fir-integration-ee139",
+  storageBucket: "fir-integration-ee139.appspot.com",
+  messagingSenderId: "318104264534",
+  appId: "1:318104264534:web:6820a006634497318300f7",
+  measurementId: "G-6G1HNZD632"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-});
-
-export { app, auth };
+const analytics = getAnalytics(app);
